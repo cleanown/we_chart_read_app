6e18bd21b1312a9f59647e7f9032ed33997882c0
cleanown <1346387492@qq.com>
2021-02-07 18:05:33 +0800

    去除右上角debugger

--- lib/main.dart
+++ lib/main.dart
@@ -9,6 +9,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: '微信读书',
       theme: ThemeData(
         primarySwatch: Colors.blue,
